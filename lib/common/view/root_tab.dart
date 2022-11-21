1c77b85ef756367263c059d84bd980b2a5d6bffc
ace <5886230+DAEWOONPARK@users.noreply.github.com>
2022-11-21 17:15:50 +0900

    - AppBar, TabBar 만들기

--- lib/common/view/root_tab.dart
+++ lib/common/view/root_tab.dart
@@ -2,16 +2,55 @@ import 'package:actual/common/const/colors.dart';
 import 'package:actual/common/layout/default_layout.dart';
 import 'package:flutter/material.dart';
 
-class RootTab extends StatelessWidget {
+class RootTab extends StatefulWidget {
   const RootTab({Key? key}) : super(key: key);
+
+  @override
+  State<RootTab> createState() => _RootTabState();
+}
+
+class _RootTabState extends State<RootTab> {
+  int index = 0;
 
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
+      title: '코팩 딜리버',
       child: Center(
         child: Text(
           'RootTab',
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: PRIMARY_COLOR,
+        unselectedItemColor: BODY_TEXT_COLOR,
+        selectedFontSize: 10,
+        unselectedFontSize: 10,
+        type: BottomNavigationBarType.fixed, // shifting을 사용하면 탭 눌렀을 때 확대 애니메이션 효과를 볼 수 있음
+        onTap: (int index){
+          setState(() {
+            this.index = index;
+          });
+        },
+        currentIndex: index,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: '홈',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.fastfood_outlined),
+            label: '음식',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.receipt_long_outlined),
+            label: '주문',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline_outlined),
+            label: '프로필',
+          ),
+        ],
       ),
     );
   }
