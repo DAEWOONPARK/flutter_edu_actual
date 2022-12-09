f4633a0179502ca2c3170f421507e946bd667c63
ace <5886230+DAEWOONPARK@users.noreply.github.com>
2022-12-09 16:44:52 +0900

    CursorPagination 상태 작업

--- lib/common/model/cursor_pagination_model.dart
+++ lib/common/model/cursor_pagination_model.dart
@@ -1,13 +1,23 @@
 import 'package:json_annotation/json_annotation.dart';
 
-import '../../restaurant/model/restaurant_model.dart';
-
 part 'cursor_pagination_model.g.dart';
+
+abstract class CursorPaginationBase {}
+
+class CursorPaginationError extends CursorPaginationBase {
+  final String message;
+
+  CursorPaginationError({
+    required this.message,
+  });
+}
+
+class CursorPaginationLoading extends CursorPaginationBase {}
 
 @JsonSerializable(
   genericArgumentFactories: true,
 )
-class CursorPagination<T> {
+class CursorPagination<T> extends CursorPaginationBase {
   final CursorPaginationMeta meta;
   final List<T> data;
 
@@ -33,4 +43,20 @@ class CursorPaginationMeta {
 
   factory CursorPaginationMeta.fromJson(Map<String, dynamic> json) =>
       _$CursorPaginationMetaFromJson(json);
+}
+
+// 새로 고침할 때
+class CursorPaginationRefetching extends CursorPagination {
+  CursorPaginationRefetching({
+    required super.meta,
+    required super.data,
+  });
+}
+
+// 리스트 맨 아래로 내려서 추가 데이터를 요청하는 중
+class CursorPaginationFetchingMore extends CursorPagination {
+  CursorPaginationFetchingMore({
+    required super.meta,
+    required super.data,
+  });
 }
