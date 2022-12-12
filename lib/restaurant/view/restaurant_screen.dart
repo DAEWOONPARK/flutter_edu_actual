13b9e6c0612833034c79bba936547de8cec681d6
ace <5886230+DAEWOONPARK@users.noreply.github.com>
2022-12-12 15:27:50 +0900

    FetchMore 로딩 UI 작업

--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -23,7 +23,7 @@ class _RestaurantScreenState extends ConsumerState<RestaurantScreen> {
 
   void scrollListener() {
     // 현재 위치가 최대 길이보다 조금 덜 되는 위치까지 왔다면, 새로운 데이터를 추가 요청
-    if(controller.offset > controller.position.maxScrollExtent - 300) {
+    if (controller.offset > controller.position.maxScrollExtent - 300) {
       ref.read(restaurantProvider.notifier).paginate(fetchMore: true);
     }
   }
@@ -33,14 +33,14 @@ class _RestaurantScreenState extends ConsumerState<RestaurantScreen> {
     final data = ref.watch(restaurantProvider);
 
     // 완전 처음 로딩일 때
-    if(data is CursorPaginationLoading) {
+    if (data is CursorPaginationLoading) {
       return Center(
         child: CircularProgressIndicator(),
       );
     }
 
     // 에러
-    if(data is CursorPaginationError) {
+    if (data is CursorPaginationError) {
       return Center(
         child: Text(data.message),
       );
@@ -56,8 +56,19 @@ class _RestaurantScreenState extends ConsumerState<RestaurantScreen> {
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
       child: ListView.separated(
         controller: controller,
-        itemCount: cp.data.length,
+        itemCount: cp.data.length + 1,
         itemBuilder: (_, index) {
+          if (index == cp.data.length) {
+            return Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+              child: Center(
+                  child: data is CursorPaginationFetchingMore
+                      ? CircularProgressIndicator()
+                      : Text('마지막 데이터 입니다 ㅠㅠ')),
+            );
+          }
+
           final pItem = cp.data[index];
 
           return GestureDetector(
