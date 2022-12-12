307903c6bcac98c6eadbde612465415305468fff
ace <5886230+DAEWOONPARK@users.noreply.github.com>
2022-12-12 17:52:08 +0900

    RatingCardHeader UI 구현

---
+++ lib/rating/component/rating_card.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+import '../../common/const/colors.dart';
+
+class RatingCard extends StatelessWidget {
+  // Network Image, Asset Image
+  final ImageProvider avatarImage; // CircleAvatar
+  final List<Image> images; // List로 위젯 이미지를 보여줄 때
+  final int rating; // 별점
+  final String email; // 이메일
+  final String content; // 리뷰 내용
+
+  const RatingCard({
+    required this.avatarImage,
+    required this.images,
+    required this.rating,
+    required this.email,
+    required this.content,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        _Header(
+          avatarImage: avatarImage,
+          rating: rating,
+          email: email,
+        ),
+        _Body(),
+        _Images(),
+      ],
+    );
+  }
+}
+
+class _Header extends StatelessWidget {
+  final ImageProvider avatarImage;
+  final int rating;
+  final String email;
+
+  const _Header({
+    required this.avatarImage,
+    required this.rating,
+    required this.email,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        CircleAvatar(
+          radius: 12.0,
+          backgroundImage: avatarImage,
+        ),
+        const SizedBox(width: 8.0,),
+        Expanded(
+          child: Text(
+            email,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(
+              fontSize: 14.0,
+              color: Colors.black,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ),
+        ...List.generate(
+          5,
+          (index) => Icon(
+            index < rating ? Icons.star : Icons.star_border_outlined,
+            color: PRIMARY_COLOR,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _Body extends StatelessWidget {
+  const _Body({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class _Images extends StatelessWidget {
+  const _Images({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
