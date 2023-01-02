fd497f2ee9cf3132e7d63c9ab3c7a759943fbbc8
ace <5886230+DAEWOONPARK@users.noreply.github.com>
2023-01-02 17:38:59 +0900

    Authentication 마무리

--- lib/common/provider/go_router.dart
+++ lib/common/provider/go_router.dart
@@ -3,7 +3,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
 final routerProvider = Provider<GoRouter>((ref) {
-  final provider = ref.watch(authProvider);
+  // read - 한 번만 읽고 값이 변경되어도 다시 빌드하지 않음
+  final provider = ref.read(authProvider);
 
   return GoRouter(
     routes: provider.routes,
--- lib/user/provider/auth_provider.dart
+++ lib/user/provider/auth_provider.dart
@@ -71,7 +71,7 @@ class AuthProvider extends ChangeNotifier {
     // UserModel, 사용자 정보가 있는 상태라면 로그인 중이거나,
     // 현재 위치가 SplashScreen이면 홈으로 이동
     if (user is UserModel) {
-      return logginIn || state.location == 'splash' ? '/' : null;
+      return logginIn || state.location == '/splash' ? '/' : null;
     }
 
     // UserModelError
