d5fb38fb56b428bec9f0790aa700906dd75e5a06
ace <5886230+DAEWOONPARK@users.noreply.github.com>
2023-01-05 16:25:02 +0900

    BasketProvider 작업

--- lib/user/repository/user_me_repository.dart
+++ lib/user/repository/user_me_repository.dart
@@ -1,3 +1,5 @@
+import 'package:actual/user/model/basket_item_model.dart';
+import 'package:actual/user/model/patch_basket_body.dart';
 import 'package:actual/user/model/user_model.dart';
 import 'package:dio/dio.dart' hide Headers;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -24,4 +26,14 @@ abstract class UserMeRepository {
   @GET('/')
   @Headers({'accessToken': 'true'})
   Future<UserModel> getMe();
+
+  @GET('/basket')
+  @Headers({'accessToken': 'true'})
+  Future<List<BasketItemModel>> getBasket();
+
+  @PATCH('/basket')
+  @Headers({'accessToken': 'true'})
+  Future<List<BasketItemModel>> patchBasket({
+    @Body() required PatchBasketBody body,
+  });
 }
