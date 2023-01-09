c44772390e02e71da67732f43fe3303f9f31574d
ace <5886230+DAEWOONPARK@users.noreply.github.com>
2023-01-09 11:30:13 +0900

    - PatchBasketBody를 List 형태로 수정 - 장바구니 기능 Optimistic Response 적용

--- lib/user/provider/basket_provider.dart
+++ lib/user/provider/basket_provider.dart
@@ -1,15 +1,31 @@
 import 'package:actual/product/model/product_model.dart';
 import 'package:actual/user/model/basket_item_model.dart';
+import 'package:actual/user/model/patch_basket_body.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:collection/collection.dart';
 
+import '../repository/user_me_repository.dart';
+
 final basketProvider =
     StateNotifierProvider<BasketProvider, List<BasketItemModel>>((ref) {
-  return BasketProvider();
+      final repository = ref.watch(userMeRepositoryProvider);
+  return BasketProvider(repository: repository);
 });
 
 class BasketProvider extends StateNotifier<List<BasketItemModel>> {
-  BasketProvider() : super([]);
+  final UserMeRepository repository;
+
+  BasketProvider({required this.repository,}) : super([]);
+
+  Future<void> patchBasket() async {
+    await repository.patchBasket(
+        body: PatchBasketBody(
+          basket: state.map((e) =>
+              PatchBasketBodyBasket(productId: e.product.id, count: e.count,)
+          ).toList(),
+        )
+    );
+  }
 
   Future<void> addToBasket({
     required ProductModel product,
@@ -24,49 +40,58 @@ class BasketProvider extends StateNotifier<List<BasketItemModel>> {
       state = state
           .map(
             (e) =>
-                e.product.id == product.id ? e.copyWith(count: e.count + 1) : e,
-          )
+        e.product.id == product.id ? e.copyWith(count: e.count + 1) : e,
+      )
           .toList();
     } else {
       state = [...state, BasketItemModel(product: product, count: 1)];
     }
-  }
 
-  Future<void> removeFromBasket({
-    required ProductModel product,
-    // true이면 count와 관계없이 삭제한다.
-    bool isDelete = false,
-  }) async {
-    // 1) 장바구니에 상품이 존재할 때
-    //    1) 상품의 카운트가 1보다 크면 -1한다.
-    //    2) 상품의 카운트가 1이면 삭제한다.
-    // 2) 상품이 존재하지 않을 때, 즉시 함수 반환하고 아무것도 하지 않는다.
+    // 에러가 발생할 가능성이 작고, 에러가 나더라도 큰 문제가 없다면
+    // 굳이 캐싱 업데이트를 서버 요청 후에 하는 것보단, 캐싱 업데이트를 먼저 해주는게
+    // 사용자에게 빠르게 동작하는 앱이라는 경험할 수 있게 한다.
+    // -> Optimistic Response(긍정적 응답): 응답이 성공할 것이라고 가정하고 상태를 먼저 업데이트 함
+    await patchBasket();
 
-    final exists =
-        state.firstWhereOrNull((e) => e.product.id == product.id) != null;
+    Future<void> removeFromBasket({
+      required ProductModel product,
+      // true이면 count와 관계없이 삭제한다.
+      bool isDelete = false,
+    }) async {
+      // 1) 장바구니에 상품이 존재할 때
+      //    1) 상품의 카운트가 1보다 크면 -1한다.
+      //    2) 상품의 카운트가 1이면 삭제한다.
+      // 2) 상품이 존재하지 않을 때, 즉시 함수 반환하고 아무것도 하지 않는다.
 
-    if (!exists) {
-      return;
-    }
+      final exists =
+          state.firstWhereOrNull((e) => e.product.id == product.id) != null;
 
-    final existingProduct = state.firstWhere((e) => e.product.id == product.id);
+      if (!exists) {
+        return;
+      }
 
-    if (existingProduct.count == 1 || isDelete) {
-      state = state
-          .where(
-            (e) => e.product.id != product.id,
+      final existingProduct = state.firstWhere((e) =>
+      e.product.id == product.id);
+
+      if (existingProduct.count == 1 || isDelete) {
+        state = state
+            .where(
+              (e) => e.product.id != product.id,
+        )
+            .toList();
+      } else {
+        state = state
+            .map(
+              (e) =>
+          e.product.id == product.id
+              ? e.copyWith(
+            count: e.count - 1,
           )
-          .toList();
-    } else {
-      state = state
-          .map(
-            (e) => e.product.id == product.id
-                ? e.copyWith(
-                    count: e.count - 1,
-                  )
-                : e,
-          )
-          .toList();
+              : e,
+        )
+            .toList();
+      }
+      await patchBasket();
     }
   }
 }
