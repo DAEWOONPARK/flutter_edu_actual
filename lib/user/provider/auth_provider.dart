98062165e1a46a0ea1cda2a6f7cd9476c3fe2b51
ace <5886230+DAEWOONPARK@users.noreply.github.com>
2023-01-10 11:54:18 +0900

    결제함수 마무리

--- lib/user/provider/auth_provider.dart
+++ lib/user/provider/auth_provider.dart
@@ -8,6 +8,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
+import '../../order/view/order_done_screen.dart';
 import '../model/user_model.dart';
 
 final authProvider = ChangeNotifierProvider<AuthProvider>((ref) {
@@ -46,6 +47,11 @@ class AuthProvider extends ChangeNotifier {
           path: '/basket',
           name: BasketScreen.routeName,
           builder: (_, state) => BasketScreen(),
+        ),
+        GoRoute(
+          path: '/order_done',
+          name: OrderDoneScreen.routeName,
+          builder: (_, state) => OrderDoneScreen(),
         ),
         GoRoute(
           path: '/splash',
