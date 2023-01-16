09a4352aac3a91acbc037fad19a32de86e82980c
ace <5886230+DAEWOONPARK@users.noreply.github.com>
2023-01-16 16:15:56 +0900

    OrderCard 작업

--- lib/order/repository/order_repository.dart
+++ lib/order/repository/order_repository.dart
@@ -1,10 +1,13 @@
 import 'package:actual/common/dio/dio.dart';
+import 'package:actual/common/model/pagination_params.dart';
+import 'package:actual/common/repository/base_pagination_repository.dart';
 import 'package:actual/order/model/post_order_body.dart';
 import 'package:dio/dio.dart' hide Headers;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:retrofit/http.dart';
 
 import '../../common/const/data.dart';
+import '../../common/model/cursor_pagination_model.dart';
 import '../model/order_model.dart';
 
 part 'order_repository.g.dart';
@@ -18,8 +21,16 @@ final orderRepositoryProvider = Provider<OrderRepository>(
 
 // http://$ip/order
 @RestApi()
-abstract class OrderRepository {
+abstract class OrderRepository implements IBasePaginationRepository<OrderModel> {
   factory OrderRepository(Dio dio, {String baseUrl}) = _OrderRepository;
+
+  @GET('/')
+  @Headers({
+    'accessToken': 'true',
+  })
+  Future<CursorPagination<OrderModel>> paginate({
+    @Queries() PaginationParams? paginationParams = const PaginationParams(),
+  });
 
   @POST('/')
   @Headers({
